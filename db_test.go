c7e0223e5d7712d9257746dfa56665c4810dc7ce
xiaofan-luan <xiaofan.luan@zilliz.com>
2021-09-06 18:18:49 +0800

    support higher version rocksdb
    Signed-off-by: xiaofan-luan <xiaofan.luan@zilliz.com>

--- db_test.go
+++ db_test.go
@@ -210,15 +210,15 @@ func TestDBGetApproximateSizes(t *testing.T) {
 	defer db.Close()
 
 	// no ranges
-	sizes := db.GetApproximateSizes(nil)
+	sizes, _:= db.GetApproximateSizes(nil)
 	ensure.DeepEqual(t, len(sizes), 0)
 
 	// range will nil start and limit
-	sizes = db.GetApproximateSizes([]Range{{Start: nil, Limit: nil}})
+	sizes, _ = db.GetApproximateSizes([]Range{{Start: nil, Limit: nil}})
 	ensure.DeepEqual(t, sizes, []uint64{0})
 
 	// valid range
-	sizes = db.GetApproximateSizes([]Range{{Start: []byte{0x00}, Limit: []byte{0xFF}}})
+	sizes, _ = db.GetApproximateSizes([]Range{{Start: []byte{0x00}, Limit: []byte{0xFF}}})
 	ensure.DeepEqual(t, sizes, []uint64{0})
 }
 
@@ -232,14 +232,14 @@ func TestDBGetApproximateSizesCF(t *testing.T) {
 	ensure.Nil(t, err)
 
 	// no ranges
-	sizes := db.GetApproximateSizesCF(cf, nil)
+	sizes, _ := db.GetApproximateSizesCF(cf, nil)
 	ensure.DeepEqual(t, len(sizes), 0)
 
 	// range will nil start and limit
-	sizes = db.GetApproximateSizesCF(cf, []Range{{Start: nil, Limit: nil}})
+	sizes, _ = db.GetApproximateSizesCF(cf, []Range{{Start: nil, Limit: nil}})
 	ensure.DeepEqual(t, sizes, []uint64{0})
 
 	// valid range
-	sizes = db.GetApproximateSizesCF(cf, []Range{{Start: []byte{0x00}, Limit: []byte{0xFF}}})
+	sizes, _ = db.GetApproximateSizesCF(cf, []Range{{Start: []byte{0x00}, Limit: []byte{0xFF}}})
 	ensure.DeepEqual(t, sizes, []uint64{0})
 }
